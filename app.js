f2c9426ad4c474ccd4869468a1b18f144c500251
joshMonarch <sesitado@gmail.com>
2025-07-01 21:49:53 +0200

    Deleted unused code lines

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 window.onload = function () {
-  const post = document.querySelector(".post");
-  const postBody = document.querySelector(".post-body");
   const description = document.querySelector(".clip");
   const seeMore = document.querySelector(".see-more");
 
